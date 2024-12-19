5ecea79a5751b8e791bb323036a8c200a43b6018
Amin Abyaneh <amin.soleimaniabyaneh@mail.mcgill.ca>
2024-12-19 17:18:39 -0500

    Home and gripper actions

--- jaco_xbox_controller/scripts/xbox_reader.py
+++ jaco_xbox_controller/scripts/xbox_reader.py
@@ -1,150 +1,56 @@
 #!/usr/bin/env python3
-#
-# import rospy
-# from sensor_msgs.msg import Joy
-# import enum
-#
-# def joy_callback(msg):
-#     # Map Axes
-#     axes_mapping = {
-#         0: "Left Stick Horizontal",
-#         1: "Left Stick Vertical",
-#         2: "Left Trigger",
-#         3: "Right Stick Horizontal",
-#         4: "Right Stick Vertical",
-#         5: "Right Trigger",
-#         6: "D-Pad Horizontal",
-#         7: "D-Pad Vertical"
-#     }
-#
-#     # Map Buttons
-#     buttons_mapping = {
-#         0: "A",
-#         1: "B",
-#         2: "X",
-#         3: "Y",
-#         4: "Left Bumper",
-#         5: "Right Bumper",
-#         6: "Back",
-#         7: "Start",
-#         8: "Xbox Button",
-#         9: "Left Stick Click",
-#         10: "Right Stick Click"
-#     }
-#
-#     # Print Axes with Descriptions
-#     rospy.loginfo("Axes:")
-#     for i, value in enumerate(msg.axes):
-#         axis_name = axes_mapping.get(i, f"Unknown Axis {i}")
-#         rospy.loginfo(f"  {axis_name}: {value}")
-#
-#     # Print Buttons with Descriptions
-#     rospy.loginfo("Buttons:")
-#     for i, value in enumerate(msg.buttons):
-#         button_name = buttons_mapping.get(i, f"Unknown Button {i}")
-#         rospy.loginfo(f"  {button_name}: {'Pressed' if value else 'Released'}")
-#
-# def xbox_reader():
-#     rospy.init_node('xbox_reader', anonymous=True)
-#     rospy.Subscriber('/joy', Joy, joy_callback)
-#     rospy.loginfo("Xbox controller reader started. Listening on /joy...")
-#     rospy.spin()
-#
-# if __name__ == '__main__':
-#     try:
-#         xbox_reader()
-#     except rospy.ROSInterruptException:
-#         pass
-
-
-#!/usr/bin/env python3
-
-
-#!/usr/bin/env python3
-
-# import rospy
-# from sensor_msgs.msg import Joy
-# from kinova_msgs.msg import PoseVelocity  # Import for Cartesian velocity messages
-
-# # Scaling factor for very slow velocity (m/s)
-# SLOW_VELOCITY_SCALE = 20.0  # Adjusted for even slower movement
-
-# class XboxVelocityController:
-#     def __init__(self):
-#         # Initialize the ROS node
-#         rospy.init_node('xbox_velocity_controller', anonymous=True)
-
-#         # Initialize the publisher for Cartesian velocity
-#         self.velocity_pub = rospy.Publisher('/j2s7s300_driver/in/cartesian_velocity', PoseVelocity, queue_size=10)
-
-#         # Initialize the subscriber for the Xbox controller
-#         rospy.Subscriber('/joy', Joy, self.joy_callback)
-
-#         # Initialize the last known velocities
-#         self.linear_x = 0.0
-#         self.linear_y = 0.0
-#         self.linear_z = 0.0
-#         self.angular_x = 0.0
-#         self.angular_y = 0.0
-#         self.angular_z = 0.0
-
-#         # Set the rate to 100 Hz
-#         self.rate = rospy.Rate(100)  # 100 Hz
-
-#         rospy.loginfo("Xbox velocity controller started. Listening on /joy...")
-
-#     def joy_callback(self, msg):
-#         # Update velocities based on joystick input
-#         # Left stick horizontal controls linear x
-#         self.linear_x = msg.axes[0] * SLOW_VELOCITY_SCALE
-#         # Left stick vertical controls linear y
-#         self.linear_y = msg.axes[1] * SLOW_VELOCITY_SCALE
-
-#         # Right stick horizontal controls angular z (rotation around z-axis)
-#         self.angular_z = msg.axes[3] * SLOW_VELOCITY_SCALE
-
-#         # Left and Right Triggers control linear z (up/down)
-#         # Assuming triggers range from -1 (released) to 1 (fully pressed)
-#         # Adjust accordingly if your controller's trigger values differ
-#         left_trigger = (1 - msg.axes[2]) / 2  # Normalize to 0 (released) to 1 (pressed)
-#         right_trigger = (1 - msg.axes[5]) / 2
-#         self.linear_z = (right_trigger - left_trigger) * SLOW_VELOCITY_SCALE
-
-#     def run(self):
-#         while not rospy.is_shutdown():
-#             # Create a PoseVelocity message
-#             velocity_msg = PoseVelocity()
-#             velocity_msg.twist_linear_x = self.linear_x
-#             velocity_msg.twist_linear_y = self.linear_y
-#             velocity_msg.twist_linear_z = self.linear_z
-#             velocity_msg.twist_angular_x = self.angular_x
-#             velocity_msg.twist_angular_y = self.angular_y
-#             velocity_msg.twist_angular_z = self.angular_z
-
-#             # Publish the velocity message
-#             self.velocity_pub.publish(velocity_msg)
-
-#             # Sleep to maintain 100 Hz rate
-#             self.rate.sleep()
-
-# if __name__ == '__main__':
-#     try:
-
-
-
-#!/usr/bin/env python3
 
 import rospy
+
+import kinova_msgs
+import actionlib
+
 from sensor_msgs.msg import Joy
 from kinova_msgs.msg import PoseVelocity
 
-SLOW_VELOCITY_SCALE = 20.0
+from kinova_msgs.srv import HomeArm
+
+SLOW_VELOCITY_SCALE = 1.0
+
 
 class XboxVelocityController:
+    """ Xbox controller with the following code.
+
+    | **Index** | **Button Name on the Actual Controller** |
+    |-----------|-----------------------------------------|
+    | 0         | A                                       |
+    | 1         | B                                       |
+    | 2         | X                                       |
+    | 3         | Y                                       |
+    | 4         | LB                                      |
+    | 5         | RB                                      |
+    | 6         | Back                                    |
+    | 7         | Start                                   |
+    | 8         | Power                                   |
+    | 9         | Button Stick Left                       |
+    | 10        | Button Stick Right                      |
+
+
+    | **Index** | **Axis Name on the Actual Controller** |
+    |-----------|----------------------------------------|
+    | 0         | Left/Right Axis Stick Left            |
+    | 1         | Up/Down Axis Stick Left               |
+    | 2         | Left/Right Axis Stick Right           |
+    | 3         | Up/Down Axis Stick Right              |
+    | 4         | RT                                    |
+    | 5         | LT                                    |
+    | 6         | Cross Key Left/Right                  |
+    | 7         | Cross Key Up/Down                     |
+
+    """
     def __init__(self):
         rospy.init_node('xbox_velocity_controller', anonymous=True)
-        self.velocity_pub = rospy.Publisher('/j2s7s300_driver/in/cartesian_velocity', PoseVelocity, queue_size=10)
+        self.velocity_pub = rospy.Publisher('/j2s7s300_driver/in/cartesian_velocity',
+                                            PoseVelocity, queue_size=10)
         rospy.Subscriber('/joy', Joy, self.joy_callback)
+        rospy.wait_for_service('/j2s7s300_driver/in/home_arm')
+        self.home_arm_service = rospy.ServiceProxy('/j2s7s300_driver/in/home_arm', HomeArm)
+
         self.linear_x = 0.0
         self.linear_y = 0.0
         self.linear_z = 0.0
@@ -156,17 +62,63 @@ class XboxVelocityController:
     def joy_callback(self, msg):
         self.linear_x = msg.axes[0] * SLOW_VELOCITY_SCALE
         self.linear_y = msg.axes[1] * SLOW_VELOCITY_SCALE
+
         lb_pressed = msg.buttons[4]
         rb_pressed = msg.buttons[5]
+
+        x_pressed = msg.buttons[2] # gripper close
+        y_pressed = msg.buttons[3] # gripper open
+        a_pressed = msg.buttons[0] # home position
+
         if lb_pressed and not rb_pressed:
             self.linear_z = SLOW_VELOCITY_SCALE
         elif rb_pressed and not lb_pressed:
             self.linear_z = -SLOW_VELOCITY_SCALE
         else:
             self.linear_z = 0.0
+
+        # TODO: fix multiple service call bug
+        if a_pressed:
+            self.home_arm_service()
+
+        if x_pressed and not y_pressed:
+            # close the gripper
+            self.gripper_client([5000, 5000, 5000]) # 5000 is almost close!
+        elif y_pressed and not x_pressed:
+            # open the gripper
+            self.gripper_client([0, 0, 0])
+        else:
+            pass
+
         self.angular_x = msg.axes[4] * SLOW_VELOCITY_SCALE
         self.angular_y = msg.axes[3] * SLOW_VELOCITY_SCALE
-        self.angular_z = msg.axes[3] * SLOW_VELOCITY_SCALE
+        # self.angular_z = msg.axes[3] * SLOW_VELOCITY_SCALE
+
+
+
+    def gripper_client(self, finger_positions):
+        action_address = '/j2s7s300_driver/fingers_action/finger_positions'
+
+        client = actionlib.SimpleActionClient(action_address,
+                                            kinova_msgs.msg.SetFingersPositionAction)
+        client.wait_for_server()
+
+        goal = kinova_msgs.msg.SetFingersPositionGoal()
+        goal.fingers.finger1 = float(finger_positions[0])
+        goal.fingers.finger2 = float(finger_positions[1])
+
+        if len(finger_positions) < 3:
+            goal.fingers.finger3 = 0.0
+        else:
+            goal.fingers.finger3 = float(finger_positions[2])
+        client.send_goal(goal)
+        if client.wait_for_result(rospy.Duration(5.0)):
+            return client.get_result()
+        else:
+            client.cancel_all_goals()
+            print('Failed action cancelling goals!')
+            return None
+
 
     def run(self):
         while not rospy.is_shutdown():
@@ -187,7 +139,3 @@ if __name__ == '__main__':
     except rospy.ROSInterruptException:
         pass
 
-        controller = XboxVelocityController()
-        controller.run()
-    except rospy.ROSInterruptException:
-        pass
